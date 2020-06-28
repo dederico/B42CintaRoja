eaa3dedaee222c5c8526b9ed2e6b58126aa086e9
dederico <dederico@gmail.com>
2020-06-28 16:09:08 -0500

    git:SuperClase

---
+++ Tareas/Electrodomestico.js
@@ -0,0 +1,22 @@
+// I ) Crea una superclase llamada Electrodoméstico con las
+// siguientes características:
+// a.Sus atributos son precio, color, consumoEnergetico
+// y capacidad(peso máximo)
+// b.El constructor solo debe pedir precio, color
+// y capacidad.
+//     c.consumoEnergetico debe iniciar con valor de 100
+
+class Electrodomestico {
+  constructor(precio, color, capacidad) {
+    this.precio = precio;
+    this.color = color;
+    this.capacidad = capacidad;
+  }
+  setConsumoEnergetico() {
+    this.consumoEnergetico = consumoEnergetico;
+  }
+}
+
+module.exports = {
+  Electrodomestico,
+};
